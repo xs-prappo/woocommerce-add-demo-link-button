7ade85e4eeb8ba50ba11e684365a432199e5be14
Prappo Prince <prappo.prince@gmail.com>
2020-09-23 11:08:04 +0600

    Update comment block

--- plugin.php
+++ plugin.php
@@ -2,11 +2,11 @@
 
 /**
  * Plugin Name:       Demo URL button for Woocommerce
- * Plugin URI:        http://example.com/plugin-name-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Plugin URI:        https://github.com/prappo/woocommerce-add-demo-link-button/
+ * Description:       Add custom demo link button after add to cart button in woocommerce.
  * Version:           1.0.0
  * Author:            Prappo Prince
- * Author URI:        http://example.com/
+ * Author URI:        https://prappo.dev
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       wadlb
