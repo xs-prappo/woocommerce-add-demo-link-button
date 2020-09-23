0ed94457bbbdec9ce0a2180dd9000c05161d66cd
Prappo Prince <prappo.prince@gmail.com>
2020-09-23 11:05:03 +0600

    Update comment block

--- plugin.php
+++ plugin.php
@@ -4,6 +4,18 @@
  * Plugin Name: Demo URL button for Woocommerce
  */
 
+/**
+ * Plugin Name:       Demo URL button for Woocommerce
+ * Plugin URI:        http://example.com/plugin-name-uri/
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            Prappo Prince
+ * Author URI:        http://example.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       wadlb
+ */
+
 
 /**
  * Check if WooCommerce is active
